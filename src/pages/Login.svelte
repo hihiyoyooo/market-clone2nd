524005daed1d7d4e47271357b343c38e30ba97d5
hihiyoyooo <rlagusxo7979@naver.com>
2023-09-29 14:27:31 +0900

    유저 인증까지 파이어베이스로 구현

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,15 +1,52 @@
-<form id="login-form" action="/signup" method="POST">
-  <div>로그인 페이지</div>
-  <div>
-    <label for="id">아이디</label>
-    <input type="text" id="id" name="id" required />
-  </div>
-  <div>
-    <label for="password">비밀번호</label>
-    <input type="password" id="password" name="password" required />
-  </div>
-  <div>
-    <button type="submit">로그인</button>
-  </div>
-  <div id="info" />
-</form>
+<script>
+  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  import { user$ } from "../store";
+
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const loginWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      user$.set(user);
+      localStorage.setItem("token", token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+</script>
+
+<div>
+  <div>로그인 하기</div>
+  <button class="login-btn" on:click={loginWithGoogle}>
+    <img
+      class="google-img"
+      src="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/5rH/image/aFrEyVpANu07FvoBZQbIB4aF_uc"
+      alt=""
+    />
+    <div>Google로 시작하기</div>
+    <div />
+  </button>
+</div>
+
+<style>
+  .login-btn {
+    margin-top: 10px;
+    width: 200px;
+    height: 30px;
+    border: 1px solid gray;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .google-img {
+    widows: 20px;
+    height: 20px;
+  }
+</style>
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,15 +1,68 @@
+<script>
+  import { getDatabase, ref, push } from "firebase/database";
+  import {
+    getStorage,
+    ref as refImage,
+    uploadBytes,
+    getDownloadURL,
+  } from "firebase/storage";
+  import Nav from "../components/Nav.svelte";
+
+  let title;
+  let price;
+  let description;
+  let place;
+  let files;
+
+  const storage = getStorage();
+  const db = getDatabase();
+
+  async function writeUserData(imgUrl) {
+    push(ref(db, "items/"), {
+      title,
+      price,
+      description,
+      place,
+      insertAt: new Date().getTime(),
+      imgUrl,
+    });
+    alert("게시물 등록완료!");
+    window.location.hash = "/";
+  }
+
+  const uploadFile = async () => {
+    const file = files[0];
+    const name = file.name;
+    const imgRef = refImage(storage, name);
+    await uploadBytes(imgRef, file);
+    const url = await getDownloadURL(imgRef);
+    return url;
+  };
+
+  const handleSubmit = async () => {
+    const url = await uploadFile();
+    writeUserData(url);
+  };
+</script>
+
 <h2>내 물건 팔기</h2>
-<form id="write-form">
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
   <div class="write-img__label">
     <label for="image">이미지</label>
     <div>
-      <input type="file" id="image" name="image" />
+      <input type="file" bind:files id="image" name="image" />
     </div>
   </div>
   <div class="write-title__label">
     <label for="title">제목</label>
     <div>
-      <input type="text" id="title" name="title" placeholder="제목" />
+      <input
+        type="text"
+        id="title"
+        name="title"
+        placeholder="제목"
+        bind:value={title}
+      />
     </div>
   </div>
   <div class="write-price__label">
@@ -20,6 +73,7 @@
         id="price"
         name="price"
         placeholder="₩ 가격을 입력해주세요."
+        bind:value={price}
       />
     </div>
   </div>
@@ -31,16 +85,25 @@
         id="description"
         name="description"
         placeholder="내용을 작성해주세요"
+        bind:value={description}
       />
     </div>
   </div>
   <div class="write-place__label">
     <label for="place">장소</label>
     <div>
-      <input type="text" id="place" name="place" placeholder="거래 희망 장소" />
+      <input
+        type="text"
+        id="place"
+        name="place"
+        placeholder="거래 희망 장소"
+        bind:value={place}
+      />
     </div>
   </div>
   <div id="write-btn">
     <button type="submit">작성 완료</button>
   </div>
 </form>
+
+<Nav location="write" />
